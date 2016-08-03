a9ccfed0fc97ea717df83f96a060ce27d63ad90a
845968074 <845968074@qq.com>
2016-08-03 10:05:09 +0800

    test3

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,8 +1,8 @@
-let {generateTodoHtml} = require('../src/main.js');
+let {generateTodoHtml}= require('../src/main.js');
 
 describe('string-test3', () => {
 
-  it('generate todo html with some items selected', () => {
+  fit('generate todo html with some items selected', () => {
     let todoItems = [
       {name: '吃饭', completed: true},
       {name: '练习', completed: false},
@@ -48,7 +48,7 @@ describe('string-test3', () => {
         <button>Clear completed</button>
     </footer>
 </section>
-`
+`;require(`fs`).writeFileSync('./2.txt',expected);
     expect(result).toEqual(expected);
   });
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,37 @@
 function generateTodoHtml(todoItems) {
-  return /**/;
+  let result="";
+  todoItems.map(todoItem =>{
+result+= ` <li>
+               <div><input type="checkbox" checked="checked"/><label>${todoItem.name}</label></div>
+               <form><input type="text"/></form>
+          </li>
+`;});
+  let expected = `
+<section>
+    <header>
+        <h1>todos</h1>
+        <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>
+    </header>
+    <section>
+        <input type="checkbox"/>
+        <label for="toggle-all">Mark all as complete</label>
+        <ul>
+           ${result.trim()}
+</ul>
+    </section>
+    <footer>
+        <strong>2</strong> items left
+        <ul>
+            <li><a href="#/">All</a></li>
+            <li><a href="#/active">Active</a></li>
+            <li><a href="#/completed">Completed</a></li>
+        </ul>
+        <button>Clear completed</button>
+    </footer>
+</section>`;
+  require(`fs`).writeFileSync('./1.txt',expected);
+  return result;
 }
 
-module.exports = {
-  generateTodoHtml: generateTodoHtml
-}
+module.exports = {generateTodoHtml
+};

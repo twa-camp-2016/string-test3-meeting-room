1fd107b00e3748883daca07e978414c4b00bba13
ZhuoYueYue <2574376092@qq.com>
2016-08-03 10:07:44 +0800

    test-string3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,41 @@
 function generateTodoHtml(todoItems) {
-  return /**/;
+  return /**/
+
+  let line=`<section>\n`;
+   line +=`    <header>\n`;
+  line +=`        <h1>todos</h1>\n`;
+  line+=`        <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>\n`;
+  line+=`    </header>`;
+  line+=`    <section>`;
+  line += `        <input type="checkbox"/>`;
+  line+=`        <label for="toggle-all">Mark all as complete</label>`;
+  line +=`        <ul>`;
+  for(let item of todoItems){
+    if(item.completed===true){
+    line +=`            <li>`;
+    line += `                <div><input type="checkbox" checked="checked"/><label>${item.name}</label></div>`;
+    line+=`                <form><input type="text"/></form>`;
+    line += `            </li>`;}
+    else{
+      line +=`            <li>`;
+      line += `                <div><input type="checkbox"/><label>${item.name}</label></div>`;
+      line+=`                <form><input type="text"/></form>`;
+      line += `            </li>`
+    }
+  }
+  line +=`</ul>
+    </section>
+    <footer>
+        <strong>2</strong> items left
+        <ul>
+            <li><a href="#/">All</a></li>
+            <li><a href="#/active">Active</a></li>
+            <li><a href="#/completed">Completed</a></li>
+        </ul>
+        <button>Clear completed</button>
+    </footer>
+</section>`
+  return line;
 }
 
 module.exports = {

f86b56ef091f7c1c2c6a4323441922b8d05bc922
jydawn <1198621249@qq.com>
2016-08-03 10:16:32 +0800

    string-test3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,47 @@
 function generateTodoHtml(todoItems) {
-  return /**/;
+  let count=0;
+  for(let item of todoItems){
+    if(item.completed===false){
+      count+=1;
+    }
+  }
+  for(let item of todoItems){
+    if(item.completed===true){
+     let lines=`   
+    <li>
+    <div><input type="checkbox" checked="checked"/><label>${item.name}</label></div>
+        <form><input type="text"/></form>
+        </li>
+  `
+  }else {
+   let lines=` <li>
+      <div><input type="checkbox"/><label>${item.name}</label></div>
+          <form><input type="text"/></form>
+          </li>`;
+    }
+  return expected = `
+<section>
+    <header>
+        <h1>todos</h1>
+        <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>
+    </header>
+    <section>
+        <input type="checkbox"/>
+        <label for="toggle-all">Mark all as complete</label>
+        
+    </section>
+    <footer>
+        <strong>count</strong> items left
+        <ul>
+            <li><a href="#/">All</a></li>
+            <li><a href="#/active">Active</a></li>
+            <li><a href="#/completed">Completed</a></li>
+        </ul>
+        <button>Clear completed</button>
+    </footer>
+</section>
+`
+      /**/;
 }
 
 module.exports = {

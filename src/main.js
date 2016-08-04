5f41934478a96082b5347c1006d035869b704a44
duancoin <duanjm104@163.com>
2016-08-04 14:34:28 +0800

    段锦梦

--- src/main.js
+++ src/main.js
@@ -1,53 +1,64 @@
 function generateTodoHtml(todoItems) {
-  let result = [];
-  result.push(`<section>
-  <header>
-    <h1>todos</h1>
-    <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>
-  </header>
-  <section>
-    <input type="checkbox"/>
-    <label for="toggle-all">Mark all as complete</label>
-    <ul>
-`);
-
-  todoItems.map(item =>{
-    let div = `      <li>
-        <div><input type="checkbox"`;
-    if(item.completed === true) {
-      div += ' checked="checked"';
-    }
-    div += `/><label>${item.name}</label></div>
-        <form><input type="text"/></form>
-      </li>
+    let result = `
+<section>
+    <header>
+        <h1>todos</h1>
+        <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>
+    </header>
+    <section>
+        <input type="checkbox"/>
+        <label for="toggle-all">Mark all as complete</label>
+        <ul>
 `;
-    result.push(div);
-  });
-  result.push(`    </ul>
-  </section>
-  <footer>`)
-  let unCompletedCount = 0;
   todoItems.map(item =>{
-    if(item.completed === false) {
-      unCompletedCount++;
-    }
+      let li = `            <li>
+                <div><input type="checkbox"`;
+      if(item.completed === true) {
+          li += ' checked="checked"';
+      }
+      li += `/><label>${item.name}</label></div>
+                <form><input type="text"/></form>
+            </li>
+`;
+      result += li;
   });
 
-  result.push(`    <strong>${unCompletedCount}</strong> items left
-    <ul>
-      <li><a href="#/">All</a></li>
-      <li><a href="#/active">Active</a></li>
-      <li><a href="#/completed">Completed</a></li>
-    </ul>
-    <button>Clear completed</button>
-  </footer>
-</section>`);
+    result += `        </ul>
+    </section>
+    <footer>
+`;
 
+    let unCompletedCount = 0;
+    todoItems.map(item =>{
+        if(item.completed === false) {
+            unCompletedCount++;
+        }
+    });
 
-  return result;
+    result += `        <strong>${unCompletedCount}</strong> items left
+        <ul>
+            <li><a href="#/">All</a></li>
+            <li><a href="#/active">Active</a></li>
+            <li><a href="#/completed">Completed</a></li>
+        </ul>
+        <button>Clear completed</button>
+    </footer>
+</section>
+`;
+    require('fs').writeFileSync( '1', result);
+
+    return result;
+
 }
-
 
 module.exports = {
-  generateTodoHtml: generateTodoHtml
+    generateTodoHtml: generateTodoHtml
 }
+let todoItems = [
+    {name: '吃饭', completed: false},
+    {name: '练习', completed: false},
+    {name: '锻炼', completed: false},
+    {name: '睡觉', completed: false}
+];
+generateTodoHtml(todoItems);
+//console.log(false && true || true);

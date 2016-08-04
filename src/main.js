6d117ca6ab7be656d1e07d2a4d4ab1aa7fc8509e
yoyo <18829290140@163.com>
2016-08-04 14:27:21 +0800

    the second prictice

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 function generateTodoHtml(todoItems) {
   let num=0;
-  let result =`
+  let result = `
 <section>
     <header>
         <h1>todos</h1>
@@ -10,18 +10,19 @@ function generateTodoHtml(todoItems) {
         <input type="checkbox"/>
         <label for="toggle-all">Mark all as complete</label>
         <ul>`;
-  todoItems.forEach(({name,completed})=>{
-    let check='';
-    if(completed===false){
-      num++;
+
+  todoItems.forEach(({name,completed })=>{
+    let checked='';
+    if(completed){
+      checked=` checked="checked"`;
     }else{
-      check+=` checked="checked"`;
+      num++;
     }
     result+=`
             <li>
-                <div><input type="checkbox"${check}/><label>${name}</label></div>
+                <div><input type="checkbox"${checked}/><label>${name}</label></div>
                 <form><input type="text"/></form>
-            </li>`;
+            </li>`
   });
   result+=`
         </ul>
@@ -39,6 +40,46 @@ function generateTodoHtml(todoItems) {
 `;
   require('fs').writeFileSync('./string.txt',result);
   return result;
+//   let num=0;
+//   let result =`
+// <section>
+//     <header>
+//         <h1>todos</h1>
+//         <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>
+//     </header>
+//     <section>
+//         <input type="checkbox"/>
+//         <label for="toggle-all">Mark all as complete</label>
+//         <ul>`;
+//   todoItems.forEach(({name,completed})=>{
+//     let check='';
+//     if(completed===false){
+//       num++;
+//     }else{
+//       check+=` checked="checked"`;
+//     }
+//     result+=`
+//             <li>
+//                 <div><input type="checkbox"${check}/><label>${name}</label></div>
+//                 <form><input type="text"/></form>
+//             </li>`;
+//   });
+//   result+=`
+//         </ul>
+//     </section>
+//     <footer>
+//         <strong>${num}</strong> items left
+//         <ul>
+//             <li><a href="#/">All</a></li>
+//             <li><a href="#/active">Active</a></li>
+//             <li><a href="#/completed">Completed</a></li>
+//         </ul>
+//         <button>Clear completed</button>
+//     </footer>
+// </section>
+// `;
+//   require('fs').writeFileSync('./string.txt',result);
+//   return result;
 }
 
 module.exports = {

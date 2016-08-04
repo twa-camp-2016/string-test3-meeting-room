9499fb802d8d8d8061ad20fd6490f5f1a5b818e8
baixue1016 <1773158403@qq.com>
2016-08-04 14:30:59 +0800

    test-string

--- src/main.js
+++ src/main.js
@@ -1,7 +1,49 @@
+let _ = require('lodash');
+
 function generateTodoHtml(todoItems) {
-  return /**/;
+    let lines = [];
+    lines.push('');
+    lines.push(`<section>`);
+    lines.push(`    <header>`);
+    lines.push(`        <h1>todos</h1>`);
+    lines.push(`        <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>`);
+    lines.push(`    </header>`);
+    lines.push(`    <section>`);
+    lines.push(`        <input type="checkbox"/>`);
+    lines.push(`        <label for="toggle-all">Mark all as complete</label>`);
+    lines.push(`        <ul>`);
+
+    for (let element of todoItems) {
+        if (element.completed === true) {
+            lines.push(`            <li>`);
+            lines.push(`                <div><input type="checkbox" checked="checked"/><label>${element.name}</label></div>`);
+            lines.push(`                <form><input type="text"/></form>`);
+            lines.push(`            </li>`);
+        } else {
+            lines.push(`            <li>`);
+            lines.push(`                <div><input type="checkbox"/><label>${element.name}</label></div>`);
+            lines.push(`                <form><input type="text"/></form>`);
+            lines.push(`            </li>`);
+        }
+    }
+
+    let number = _(todoItems).filter(x=> x.completed === false).size();
+    lines.push(`        </ul>`);
+    lines.push(`    </section>`);
+    lines.push(`    <footer>`);
+    lines.push(`        <strong>${number}</strong> items left`);
+    lines.push(`        <ul>`);
+    lines.push(`            <li><a href="#/">All</a></li>`);
+    lines.push(`            <li><a href="#/active">Active</a></li>`);
+    lines.push(`            <li><a href="#/completed">Completed</a></li>`);
+    lines.push(`        </ul>`);
+    lines.push(`        <button>Clear completed</button>`);
+    lines.push(`    </footer>`);
+    lines.push(`</section>`);
+    lines.push('');
+    return lines.join('\n');
 }
 
 module.exports = {
-  generateTodoHtml: generateTodoHtml
+    generateTodoHtml: generateTodoHtml
 }

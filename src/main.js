d33f70ec00306c69e9f768921b017d898d957a98
baixue1016 <1773158403@qq.com>
2016-08-06 14:17:01 +0800

    string-test3

--- src/main.js
+++ src/main.js
@@ -1,48 +1,49 @@
-function generateTodoHtml(todoItems) {
-let lines = [`\n`];
-  lines.push(`<section>`);
-  lines.push(`<header>`);
-  lines.push(`<h1>todos</h1>`);
-  lines.push(`<form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>`);
-  lines.push(`/<header>`);
-  lines.push(`<section>
-        <input type="checkbox"/>
-        <label for="toggle-all">Mark all as complete</label>
-        <ul>
-            <li>
-                <div><input type="checkbox" checked="checked"/><label>吃饭</label></div>
-                <form><input type="text"/></form>
-            </li>
-            <li>
-                <div><input type="checkbox"/><label>练习</label></div>
-                <form><input type="text"/></form>
-            </li>
-            <li>
-                <div><input type="checkbox" checked="checked"/><label>锻炼</label></div>
-                <form><input type="text"/></form>
-            </li>
-            <li>
-                <div><input type="checkbox"/><label>睡觉</label></div>
-                <form><input type="text"/></form>
-            </li>
-        </ul>
-</section>`);
-  lines.push(`<footer>
-        <strong>2</strong> items left
-        <ul>
-            <li><a href="#/">All</a></li>
-            <li><a href="#/active">Active</a></li>
-            <li><a href="#/completed">Completed</a></li>
-        </ul>
-        <button>Clear completed</button>
-</footer>`);
-lines.push(`</section>`);
+let _ = require('lodash');
 
-  let line = lines.join('\n');
-//  require(`fs`).writeFileSync('2.txt', line);
-  return line;
+function generateTodoHtml(todoItems) {
+    let lines = [];
+    lines.push('');
+    lines.push(`<section>`);
+    lines.push(`    <header>`);
+    lines.push(`        <h1>todos</h1>`);
+    lines.push(`        <form><input type="text" autofocus="" placeholder="What needs to be done?"/></form>`);
+    lines.push(`    </header>`);
+    lines.push(`    <section>`);
+    lines.push(`        <input type="checkbox"/>`);
+    lines.push(`        <label for="toggle-all">Mark all as complete</label>`);
+    lines.push(`        <ul>`);
+
+    for (let item of todoItems){
+        if(item.completed){
+            lines.push(`            <li>`);
+            lines.push(`                <div><input type="checkbox" checked="checked"/><label>${item.name}</label></div>`);
+            lines.push(`                <form><input type="text"/></form>`);
+            lines.push(`            </li>`);
+        }else {
+            lines.push(`            <li>`);
+            lines.push(`                <div><input type="checkbox"/><label>${item.name}</label></div>`);
+            lines.push(`                <form><input type="text"/></form>`);
+            lines.push(`            </li>`);
+        }
+    }
+
+    let number = _(todoItems).filter(x=> x.completed === false).size();
+    lines.push(`        </ul>`);
+    lines.push(`    </section>`);
+    lines.push(`    <footer>`);
+    lines.push(`        <strong>${number}</strong> items left`);
+    lines.push(`        <ul>`);
+    lines.push(`            <li><a href="#/">All</a></li>`);
+    lines.push(`            <li><a href="#/active">Active</a></li>`);
+    lines.push(`            <li><a href="#/completed">Completed</a></li>`);
+    lines.push(`        </ul>`);
+    lines.push(`        <button>Clear completed</button>`);
+    lines.push(`    </footer>`);
+    lines.push(`</section>`);
+    lines.push('');
+    return lines.join('\n');
 }
 
 module.exports = {
-  generateTodoHtml: generateTodoHtml
+    generateTodoHtml: generateTodoHtml
 }
